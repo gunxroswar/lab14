bdc61e7fd07df710e206367b7835cac8d217039b
gunxroswar <gxz.gunforce@gmail.com>
2021-12-28 21:48:39 +0700

    "เสร็จแลป14อันน่าพิศวง"

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -16,4 +16,32 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+} 
+void stat(const double A[],int N,double S[])
+{
+    double max=A[0],min=A[0];
+    double sum=0,x=0,y=1;   
+    for(int i=1;i<N;i++){
+        if(A[i]>max) max=A[i];
+        if(A[i]<min) min=A[i];
+    }
+    for(int i=0;i<N;i++){
+        sum+=A[i];
+    }
+    for(int i=0;i<N;i++){
+        x+=pow(A[i],2);
+    }
+    S[0]=sum/N;
+    S[1]=sqrt((x/N)-pow(S[0],2));
+    for(int i=0;i<N;i++){
+        y*=A[i];
+    }
+    S[2]=pow(y,double(1)/N);
+    sum=0;
+    for(int i=0;i<N;i++){
+        sum+= (1/A[i]);
+    }
+    S[3]=N/sum;
+    S[4]=max;
+    S[5]=min;
 }
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -15,9 +15,38 @@ int main()
     int s,x,y;
     do{
         showImage(image);
-        cout << "Input your brush size and location: ";
+        cout << "\nInput your brush size and location: ";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
     }while(s != 0 || x != 0 || y != 0);
     return 0;
+}
+
+void updateImage(bool image[N][M],int z,int x,int y)
+{
+    for(int i= 0;i < N;i++){
+        for(int j = 0;j < M;j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2))+1 <= z){
+                image[i][j]=1;
+            }
+        }
+    }
+}
+void showImage(const bool image[N][M])
+{
+    for(int i=0;i < M+2;i++){
+        cout << "-";
+    }
+    cout << "\n";
+    for(int j=0;j<N;j++){
+        cout << "|";
+        for(int k=0;k<M;k++){
+            if(image[j][k] == 0) cout << " ";
+            if(image[j][k] == 1) cout << "*";  
+        }
+        cout << "|\n";
+    }
+    for(int i=0;i < M+2;i++){
+        cout << "-";
+    }
 }
